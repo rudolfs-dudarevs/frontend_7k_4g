f3a5b846cfc083ad8241f491711316d1e7fe6e84
Viktors Latisevs <vlatisevs@gmail.com>
2022-05-11 22:21:44 +0300

    05: Viktors Latisevs

--- majasdarbi/05-majasdarbs/ViktorsLatisevs/Object/cv-as-object.js
+++ majasdarbi/05-majasdarbs/ViktorsLatisevs/Object/cv-as-object.js
@@ -1,27 +1,27 @@
-const myCvData = {
+const ViktorsLatisevs = {
     profession: "Export manager",
     fullName: "Viktors Latisevs",
     profileImg: "./profilepic.png",
     contactInfo: {
         tel: "29824900",
         email: "vlatisevs@gmail.com",
-        linkedIn: "https://www.linkedin.com/in/the-victor-749520204/",
+        linkedIn: "https://shorturl.ae/h3DrB",
     },
     workExperience: [
         {
             companyName: "RSJ GLOBAL MARKETING(UK)",
             positionHeld: "Field representative, Sales team builder,Customer service",
-            employementPeriod: "May 8,2013 - Novemer 14,2018",
+            employementPeriod: "May 8/2013/ - Novemer 14/2018/",
         },
         {
             companyName: "Pack Expert OU",
             positionHeld: "Export Manager",
-            employementPeriod: "July 7,2020-Present",
+            employementPeriod: "July 7/2020/-Present",
         },
         {
             companyName: "Bunker Esports powered by Republic of Gamers (BKROG)",
             positionHeld: "Support lane player/Team Captain",
-            employementPeriod: "February 25,2020-Present",
+            employementPeriod: "February 25/2020/-Present",
         }
 
     ],
@@ -32,32 +32,74 @@ const myCvData = {
         },
         {
             educationPlace: "riga secondary school n96",
+            educationPeriod: "2000-2010",
+
         },
     ],
-    skills: {
-        
-        workSkill: "customer service",
-        workSkill: "marketing",
-        workskill: "digital marketing",
-        workSkill: "designing",
-        workskill: "cyberathlete",
-
+    skills: [
+    {    
+        workSkill: "Customer service",
     },
-    faq: {
-        hobbies: "traveling/backpacking",
-        hobbies: "gaming",
-        hobbies: "streaming",
-        hobbies: "photoshop design",
+    {
+        workSkill: "Marketing",
+    },
+    {
+        workskill: "Digital marketing",
+    },
+    {
+        workSkill: "Designing",
+    },
+    {
+        workSkill: "Cyberathlete",
+    },
+
+    ],
+    faq: [
+    {    
+        hobbies: "Traveling/Backpacking",
+    },
+    {
+        hobbies: "Gaming",
+    },
+    {
+        hobbies: "Streaming",
+    },
+    {
+        hobbies: "Photoshop design",
+    },
+    {
         myFavPlaylist: "https://open.spotify.com/embed/playlist/15beV27OZkisXOzyGw5a9N"
-
     },
-    languages: {
+
+    ],
+    languages: [
+    {
         lang: "Russian",
+    },
+    {
         lang: "Latvian",
+    },
+    {
         lang: "English",
+    },
+    {
         lang: "French",
+    },
+    {
         lang: "German",
-    }
+    },
+    ]
     
 
 };
+
+console.log("Name:", ViktorsLatisevs.fullName)
+console.log("Currently working as:", ViktorsLatisevs.profession)
+console.log(`Contact info: cell number ${ViktorsLatisevs.contactInfo.tel}, email: ${ViktorsLatisevs.contactInfo.email}, linkedIn: ${ViktorsLatisevs.contactInfo.linkedIn}.`)
+console.log(`Companies worked in: ${ViktorsLatisevs.workExperience[0].employementPeriod} ${ViktorsLatisevs.workExperience[0].companyName}, ${ViktorsLatisevs.workExperience[1].employementPeriod} ${ViktorsLatisevs.workExperience[1].companyName}, ${ViktorsLatisevs.workExperience[2].employementPeriod} ${ViktorsLatisevs.workExperience[2].companyName}`)
+console.log(`Areas worked in: ${ViktorsLatisevs.workExperience[0].positionHeld}, ${ViktorsLatisevs.workExperience[1].positionHeld}, ${ViktorsLatisevs.workExperience[2].positionHeld}`)
+console.log(`Education: ${ViktorsLatisevs.education[0].educationPeriod} ${ViktorsLatisevs.education[0].educationPlace} , ${ViktorsLatisevs.education[1].educationPeriod} ${ViktorsLatisevs.education[1].educationPlace}`)
+console.log(`Skills: ${ViktorsLatisevs.skills[0].workSkill}, ${ViktorsLatisevs.skills[1].workSkill}, ${ViktorsLatisevs.skills[2].workSkill}, ${ViktorsLatisevs.skills[3].workSkill}, ${ViktorsLatisevs.skills[4].workSkill}`)
+console.log(`Hobbies: ${ViktorsLatisevs.faq[0].hobbies}, ${ViktorsLatisevs.faq[1].hobbies}, ${ViktorsLatisevs.faq[2].hobbies}, ${ViktorsLatisevs.faq[3].hobbies}`)
+console.log(`Favourite playlist: ${ViktorsLatisevs.faq[4].myFavPlaylist}`)
+console.log(`Languages: ${ViktorsLatisevs.languages[0].lang}, ${ViktorsLatisevs.languages[1].lang},${ViktorsLatisevs.languages[2].lang},${ViktorsLatisevs.languages[3].lang},${ViktorsLatisevs.languages[4].lang}.    `)
