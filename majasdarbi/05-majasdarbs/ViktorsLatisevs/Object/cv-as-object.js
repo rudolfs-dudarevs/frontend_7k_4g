36b7b286045d3b80d2a46adfc8f54e9d3f86d9dc
Viktors Latisevs <vlatisevs@gmail.com>
2022-05-11 20:47:01 +0300

    05: Viktors Latisevs

---
+++ majasdarbi/05-majasdarbs/ViktorsLatisevs/Object/cv-as-object.js
@@ -0,0 +1,63 @@
+const myCvData = {
+    profession: "Export manager",
+    fullName: "Viktors Latisevs",
+    profileImg: "./profilepic.png",
+    contactInfo: {
+        tel: "29824900",
+        email: "vlatisevs@gmail.com",
+        linkedIn: "https://www.linkedin.com/in/the-victor-749520204/",
+    },
+    workExperience: [
+        {
+            companyName: "RSJ GLOBAL MARKETING(UK)",
+            positionHeld: "Field representative, Sales team builder,Customer service",
+            employementPeriod: "May 8,2013 - Novemer 14,2018",
+        },
+        {
+            companyName: "Pack Expert OU",
+            positionHeld: "Export Manager",
+            employementPeriod: "July 7,2020-Present",
+        },
+        {
+            companyName: "Bunker Esports powered by Republic of Gamers (BKROG)",
+            positionHeld: "Support lane player/Team Captain",
+            employementPeriod: "February 25,2020-Present",
+        }
+
+    ],
+    education: [
+        {
+            educationPlace: "Latvian Maritime Academy",
+            educationPeriod: "2012-2016",
+        },
+        {
+            educationPlace: "riga secondary school n96",
+        },
+    ],
+    skills: {
+        
+        workSkill: "customer service",
+        workSkill: "marketing",
+        workskill: "digital marketing",
+        workSkill: "designing",
+        workskill: "cyberathlete",
+
+    },
+    faq: {
+        hobbies: "traveling/backpacking",
+        hobbies: "gaming",
+        hobbies: "streaming",
+        hobbies: "photoshop design",
+        myFavPlaylist: "https://open.spotify.com/embed/playlist/15beV27OZkisXOzyGw5a9N"
+
+    },
+    languages: {
+        lang: "Russian",
+        lang: "Latvian",
+        lang: "English",
+        lang: "French",
+        lang: "German",
+    }
+    
+
+};
