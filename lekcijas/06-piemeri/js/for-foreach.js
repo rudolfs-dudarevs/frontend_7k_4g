3eae3f27d911894e9879ba648d01f03d825d0dc3
Rudolfs Dudarevs <mans.epats@gmail.com>
2022-05-15 21:06:54 +0300

    06: removed folder

--- lekcijas/06-piemeri/js/for-foreach.js
+++
@@ -1,9 +0,0 @@
-let frontendTools = ["CSS", "HTML", "JS", "React", undefined, "Angular", "Vue"];
-
-frontendTools.forEach((item) => {
-    console.log("frontendTools array item:", item);
-
-    for(let i = 0; i < 10; i++) {
-        console.log(`Nedted loop iteration ${i} - `, item);
-    };
-});
