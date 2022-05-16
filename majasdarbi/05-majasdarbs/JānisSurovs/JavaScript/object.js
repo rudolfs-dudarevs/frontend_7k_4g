da26b3d2191c8ff0ad7022f49ed44d60a7894d4a
jsurovs <janis.surovs@gmail.com>
2022-05-16 23:59:58 +0300

    5th done

--- majasdarbi/05-majasdarbs/JānisSurovs/JavaScript/object.js
+++ majasdarbi/05-majasdarbs/JānisSurovs/JavaScript/object.js
@@ -5,10 +5,65 @@ const JanisSurovs = {
         email: "janis.surovs@gmail.com",
     },
     education: {
-        education1: "2020-2021: Frontend courses @ webskola",
-        education2: "2012-2014: Customs and tax administration",
-        education3: "2008-2012: University of Latvia Bachelor of Social Sciences in Management",
-        education4: "1997-2008: Gulbene's Secondary School No. 2",
+        education1: {
+            Period: "2020-2021",
+            Place: "Webskola",
+            Description: "Frontend courses"
+        },
+        education2: {
+            Period: "2012-2014",
+            Place: "Riga Technical University",
+            Description: "Master's Degree Customs and tax administration"
+        },
+        education3: {
+            Period: "2008-2012",
+            Place: "University of Latvia",
+            Description: "University of Latvia Bachelor of Social Sciences in Management"
+        },
+        education4: {
+            Period: "1997-2008",
+            Place: "Gulbene's Secondary School No. 2"
+            Description: "Secondary education"
+        },
     },
-    workExperience
+    workExperience: {
+        workExperience1: {
+            Period: "2021-Present",
+            Position: "Co-owner",
+            Place: "Transport logistics company"
+        },
+        workExperience2: {
+            Period: "2009-2021",
+            Position: "Executive director",
+            Place: "Transport logistics company"
+        },
+        mainTasks: {
+            mainTask1: "Company management",
+            mainTask2: "Finance operation",
+            mainTask3: "Supply chain logistics",
+            mainTask4: "Marketing operations",
+            mainTask5: "Security operations",
+            mainTask6: "Customs operations",
+            mainTask7: "WEB development and maintenance",
+        },
+    },
+    skills: {
+        skill1: "Office 365",
+        skill2: "Digital marketing",
+        skill3: "Adobe (Acrobat, Illustrator, Photoshop)",
+        skill4: "Web design (WP, Elementor)",
+        skill5: "Project agile management",
+        skill6: "HR management",
+    },
+    interests: {
+        interest1: "Football",
+        interest2: "Sport activities",
+        interest3: "Traveling",
+        interest4: "IT",
+    },
+    languages: {
+        Latvian: "native",
+        English: "sufficient",
+        Russian: "fluent",
+    }
 }
