4b8e8064270ae9da73a8cecc214d194a8facf56d
jsurovs <janis.surovs@gmail.com>
2022-05-18 13:18:34 +0300

    swal added

---
+++ majasdarbi/06-majasdarbs/JānisSurovs/assets/js/script.js
@@ -0,0 +1,44 @@
+function getValue() {
+    let characters = ['a','b','c','d','e','f','g','h','i','j','k,','l','m','n','o','p','q','r','s','t','u','v','w','x,','y','z'];
+    let arrayId = prompt("Enter number from 0 to 25");
+    let output = document.getElementById('promptAnswer');
+    output.innerHTML = characters[arrayId];
+
+    if(arrayId > 25 || arrayId < 0 || arrayId === null || arrayId === '') {
+        Swal.fire({
+            title: 'Error!',
+            text: 'Something wrong. Please enter the value from 0 to 25',
+            icon: 'error',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Try again'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                getValue();
+              } else {
+                if (result.isDismissed) {
+                    swal.fire(
+                        'Cancelled',
+                        document.getElementById('promptAnswer').innerHTML = "Cancelled. Try again!"
+                    )
+                }
+              }
+          }) 
+    } else {
+        Swal.fire({
+            title: 'Success!',
+            text: `Success! You have entered a value ${arrayId}`,
+            icon: 'success',
+            confirmButtonText: 'OK'
+          })
+    };
+
+    document.addEventListener("keypress", (eventObject) => {
+        if (eventObject.key === characters[arrayId]) {
+            alert("Correct");
+        } else {
+            alert("Check value");
+        }
+    })
+}
