72631621e12906bbff85bfc76fb26c0e4330ec87
jsurovs <janis.surovs@gmail.com>
2022-06-07 01:10:03 +0300

    -

--- majasdarbi/08-majasdarbs/JanisSurovs/app.js
+++ majasdarbi/08-majasdarbs/JanisSurovs/app.js
@@ -5,4 +5,45 @@ function showPassFunction() {
     } else {
         pw.type = "password";
     }
+};
+
+function checkEmail () {
+    let email = document.getElementById('email');
+    var filter = /.+@gmail\.com/;
+    if (!filter.test(email.value)) {
+        alert('Please provide a valid email address');
+        email.focus;
+    }
+};
+
+function zipValidation() {
+    var forceZip = {
+        LV : ['^(LV-)?\\d{4}$'],
+        LT : ['^(LT-)?\\d{4}$'],
+        EE : ['^(EE-)?\\d{4}$'],
+    };
+
+    var country = document.getElementById("selectCountry").value;
+    var zip = document.getElementById("zip");
+
+    var checkZip = new RegExp(forceZip[country][0],"");
+    console.log(checkZip)
+};
+
+
+function printValues(){
+    var matches = [];
+    var input = document.getElementsByTagName('input');
+
+    for (var key in inputs){
+        var values = input[key].value;
+        matches.push(values);
+    }
+
+    alert(matches);
+}
+
+window.onload = function () {
+    document.getElementById("email").onchange = checkEmail;
+    document.getElementById("form").addEventListener('submit', printValues);
 }
